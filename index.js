d43d2ae49a41091c0ff3fd0e05892c48c668cbf1
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2022-12-27 10:01:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.querySelector(".button").addEventListener("click",()=>{
   if(yourName.value==="krish tiwari" || yourName.value==="roshan r" || yourName.value==="anurag chandra" || yourName.value==="aman maurya"){
     document.body.innerHTML = document.body.innerHTML.replace('Try your Luck above.', "Your love success percentage is: 100%");
   }
-  elif(yourName.value==="Shivam" || yourName.value==="Aditya" || yourName.value==="Aman"){
+  else if(yourName.value==="Shivam" || yourName.value==="Aditya" || yourName.value==="Aman"){
     document.body.innerHTML = document.body.innerHTML.replace('Try your Luck above.', "You do not deserve love, Go study");
   }
   else{
