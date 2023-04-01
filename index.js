bf0f5b45af1bc1e31bacd382383613b0fc2ee8e6
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-04-01 22:43:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var yourPartName=document.querySelectorAll("input")[1];
 
 document.querySelector(".button").addEventListener("click",()=>{
   var lovecalculator= Math.floor(Math.random()*100)+1;
-  if(yourName.value==="krish tiwari" || yourName.value==="roshan r" || yourName.value==="anurag chandra" || yourName.value==="aman maurya"){
+  if(yourName.value==="krish tiwari" || yourName.value==="roshan r" || yourName.value==="anurag chandra" || yourName.value==="aman maurya" ||yourName.value==="kunal"){
     document.body.innerHTML = document.body.innerHTML.replace('Try your Luck above.', "Your love success percentage is: 100%");
   }
-  else if(yourName.value==="Shivam" || yourName.value==="Aditya" || yourName.value==="Aman"){
+  else if(yourName.value==="Shivam" || yourName.value==="Aditya" || yourName.value==="Aman" ){
     document.body.innerHTML = document.body.innerHTML.replace('Try your Luck above.', "You do not deserve love, Go study");
   }
   else{
